ea913e48009a2b90f28e7711a185db447b961bb8
Amin HIjaz <aminhijaz2001@gmail.com>
2020-06-28 19:03:04 +0300

    favorite apps list

---
+++ app/src/Favorite.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Favorite.css'
+class Favorite extends React.Component {
+    render() {
+        return(
+        <favorite>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/YouTube_Logo.svg/1280px-YouTube_Logo.svg.png" alt="Alps" />
+  <h1>Youtube</h1>
+  <hr />
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Facebook.svg/1280px-Facebook.svg.png" alt="Alps" />
+  <h1>Facebook</h1>
+  <hr />
+  <img src="https://marketingland.com/wp-content/ml-loads/2014/07/reddit-combo-1920-800x450.png" alt="Alps" />
+  <h1>Reddit</h1>
+        </favorite>
+        )
+    }
+}
+export default Favorite
